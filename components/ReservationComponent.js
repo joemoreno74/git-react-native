e7460dd127fcfbf50cd5d223236b97aaad8c5095
Joe Moreno <joe.moreno74@gmail.com>
2021-02-16 16:08:32 +0100

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -4,6 +4,7 @@ import { Text, View, ScrollView, StyleSheet,
 import { Picker } from '@react-native-community/picker'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -37,6 +38,31 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
     render() {
         return (
@@ -104,9 +130,12 @@ class Reservation extends Component {
                                             style: 'cancel'
                                         },
                                         {
-                                            text: 'OK',
-                                            onPress: () => this.resetForm(),
-                                        },
+                                            text: 'OK', 
+                                            onPress: () => {
+                                                this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                                this.resetForm();
+                                            }
+                                        }    
                                     ],
                                     { cancelable: false }
                                 )
